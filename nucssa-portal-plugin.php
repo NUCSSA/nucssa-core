c3e18b0159349b4f58b5a51d339337a915b2b2c6
Jason Ji <lu.ji1@me.com>
2019-04-03 04:12:32 -0400

    initial commit

---
+++ nucssa-portal-plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:     NUCSSA Portal Plugin
+ * Plugin URI:      https://www.nucssa.org
+ * Description:     Plugin to support NUCSSA core features
+ * Author:          NUCSSA IT
+ * Author URI:      https://www.nucssa.org/IT
+ * Text Domain:     nucssa-portal-plugin
+ * Domain Path:     /languages
+ * Version:         1.0.0
+ *
+ * @package         NUCSSA_Portal_Plugin
+ */
+
+// prevent direct access
+defined ( 'ABSPATH' ) || exit;
