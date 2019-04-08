4c16c8678b61beb780f94cd4c04dacca33d5e318
Jason Ji <lu.ji1@me.com>
2019-04-08 00:57:59 -0400

    basic setup

--- nucssa-core.php
+++ nucssa-core.php
@@ -1,16 +1,18 @@
 <?php
 /**
- * Plugin Name:     NUCSSA Portal Plugin
+ * Plugin Name:     NUCSSA Core Plugin
  * Plugin URI:      https://www.nucssa.org
  * Description:     Plugin to support NUCSSA core features
  * Author:          NUCSSA IT
  * Author URI:      https://www.nucssa.org/IT
- * Text Domain:     nucssa-portal-plugin
+ * Text Domain:     nucssa-core
  * Domain Path:     /languages
  * Version:         1.0.0
  *
- * @package         NUCSSA_Portal_Plugin
+ * @package         NUCSSA_Core
  */
 
 // prevent direct access
 defined ( 'ABSPATH' ) || exit;
+
+include_once __DIR__ . '/vendor/autoload.php';
