2fd3c8555f2a68c2c770221ec0ec3f1e0d2a0288
Jason Ji <lu.ji1@me.com>
2019-04-17 20:22:48 -0400

    set up npm, webpack, and babel; set up PHP code live-reload

---
+++ webpack.mix.js
@@ -0,0 +1,22 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management (this is borrowed from Laravel)
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.sass('assets/scss/admin.scss', 'public/css')
+   .react('assets/js/admin.js', 'public/js')
+   .copyDirectory('assets/images/', 'public/images/')
+   .browserSync({
+     proxy: "wp.localhost",
+     files: [ '*.php', 'lib/', 'config/'],
+     open: false,
+     ghostMode: false,
+   });
