8539e0d8c40a65a41c3a51b3a8ebc6e82eb33652
Jason Ji <lu.ji1@me.com>
2019-05-15 00:03:42 -0400

    add a couple of utility functions helpful for debugging

--- lib/utils/Utils.php
+++ lib/utils/Utils.php
@@ -1,7 +1,31 @@
 <?php
 namespace NUCSSACore\Utils;
 
-class Utils
-{
+/**
+ * Utility - log info to console (browser)
+ *
+ * @param Any $data, any serializable data type
+ * @param string $label
+ *
+ * Usage:
+ * console_log($router->getRoutes(), "routes");
+ */
+function console_log($data, string $label = ''){
+  $json_data = json_encode($data);
+  echo <<<CONSOLE
+<script>
+  console.log( '$label', $json_data );
+</script>
+CONSOLE;
+}
 
+/**
+ * Add a log message to the log file (log.txt under project root dir)
+ *
+ * @param string|object $action
+ * @param string|object $message
+ */
+
+ function file_log($action, $message = ''){
+  Logger::singleton()->log_action($action, $message);
 }
