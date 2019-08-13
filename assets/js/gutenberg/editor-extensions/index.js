0c20eaf4b3bb7f3a4c7a2f68e0b8e20444ab7070
Jason Ji <lu.ji1@me.com>
2019-08-13 05:49:19 -0400

    Add capability to add an icon for each page

--- assets/js/gutenberg/editor-extensions/index.js
+++ assets/js/gutenberg/editor-extensions/index.js
@@ -1,6 +1,12 @@
 import FeaturedPostExtension from './featured-post-sidebar-extension';
+import PageIconExtension from './page-icon-sidebar-extension';
 import { registerPlugin } from '@wordpress/plugins';
 
 registerPlugin('nucssa-core-editor-extensions', {
-  render: FeaturedPostExtension
+  render: () => (
+    <>
+      <FeaturedPostExtension />
+      <PageIconExtension />
+    </>
+  )
 });
